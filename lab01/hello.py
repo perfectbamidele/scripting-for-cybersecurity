fefc9751dc794a84072af9e982fed487e8b03f9b
perfectbamidele <91192540+perfectbamidele@users.noreply.github.com>
2026-09-21 10:59:38 +0000

    Complete Lab 1

---
+++ lab01/hello.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")
+course = input("What course are you studying? ")
+area = input("What area of " + course + " interests you? ")
+
+print("Hello " + name )
+print("You are studying " + course )
+print("You are interested in " + area )
---
+++ lab01/student.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")
+student_number = input("What your student number? ")
+year = input("What year are you in? ")
+
+print("Name: " + name )
+print("Student Number: " + student_number )
+print("Year: " + year )
